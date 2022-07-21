1beea0816e33350cfefcdaa14a1eba944d791f8e
Dhruvisha Patel <Dhruvishapatel3008@gmail.com>
2022-07-21 07:55:20 +0100

    first comment

---
+++ DataType1.java
@@ -0,0 +1,75 @@
+public class DataType1 {
+    public static void main(String[] args) {
+        int A=100;
+        int a=200;
+        double B=223.12;
+        double b=203.12;
+        short C=50;
+        short c=70;
+        byte D=105;
+        byte d=123;
+        long E=10101010;
+        long e=12367689;
+        float F=223;
+        float f=456;
+        char G=8;
+        char g=9;
+
+        //use integer datatype
+        System.out.println("Integer Datatype Example");
+        System.out.println("sum of A+a="+(A+a));  //300
+        System.out.println("sub of A-a="+(A-a));  //-100
+        System.out.println("mul of A*a="+(A*a));   //20000
+        System.out.println("div of A/a="+(A/a));  //0.5
+        System.out.println("mod of A%a="+(A%a));  //100
+
+        //use double datatype
+        System.out.println("Double Datatype Example");
+        System.out.println("sum of B+b="+(B+b));  //426.24
+        System.out.println("sub of B-b="+(B-b));  //20
+        System.out.println("mul of B*b="+(B*b));  //45320.1344
+        System.out.println("div of B/b="+(B/b)); //1.09846396
+        System.out.println("mod of B%b="+(B%b));  //223.12
+
+        //use short datatype
+        System.out.println("Short Datatype Example");
+        System.out.println("sum of C+c="+(C+c));  //120
+        System.out.println("sub of C-c="+(C-c));  //-20
+        System.out.println("mul of C*c="+(C*c));  //3500
+        System.out.println("div of C/c="+(C/c)); //0.714
+        System.out.println("mod of C%c="+(C%c));  //50
+
+        //use byte datatype
+        System.out.println("Byte Datatype Example");
+        System.out.println("sum of D+d="+(D+d));  //228
+        System.out.println("sub of D-d="+(D-d));  //-18
+        System.out.println("mul of D*d="+(D*d));  //12915
+        System.out.println("div of D/d="+(D/d)); //0.85365854
+        System.out.println("mod of D%d="+(D%d));  //105
+
+        //use long datatype
+        System.out.println("long Datatype Example");
+        System.out.println("sum of E+e="+(E+e));  //22468699
+        System.out.println("sub of E-e="+(E-e));  //-2266679
+        System.out.println("mul of E*e="+(E*e));  //1.24926e14
+        System.out.println("div of E/e="+(E/e)); //0.81672574
+        System.out.println("mod of E%e="+(E%e));  //10101010
+
+        //use float datatype
+        System.out.println("Float Datatype Example");
+        System.out.println("sum of F+f="+(F+f));  //679
+        System.out.println("sub of F-f="+(F-f));  //-233
+        System.out.println("mul of F*f="+(F*f));  //101688
+        System.out.println("div of F/f="+(F/f)); //0.48903509
+        System.out.println("mod of F%f="+(F%f));  //223
+
+        //use char datatype
+        System.out.println("Char Datatype Example");
+        System.out.println("sum of G+g="+(G+g));  //17
+        System.out.println("sub of G-g="+(G-g));  //-1
+        System.out.println("mul of G*g="+(G*g));  //72
+        System.out.println("div of G/g="+(G/g)); //0.88888889
+        System.out.println("mod of G%g="+(G%g)); //8
+
+    }
+}
---
+++ JavaOperations.java
@@ -0,0 +1,20 @@
+public class JavaOperations {
+
+    public static void main(String[] args) {
+
+        int A=40;
+        int B=60;
+        System.out.println("sum of A&B="+((A+B)));      //100
+        System.out.println("sub of A&B="+((A-B)));   //-20
+        System.out.println("div of A&B="+((A/B)));    //0.666666
+        System.out.println("mul of A&B="+((A*B)));    //2400
+        System.out.println("PostIncrement of A="+((A++)));  //40
+        System.out.println( "PostIncrement of B="+((B++)));  //60
+        System.out.println("PostDecrement of A="+((A--))); //41
+        System.out.println("PostDecrement of B="+((B--)));  //61
+        System.out.println("PreIncrement of A="+((++A)));  //41
+        System.out.println( "PreIncrement of B="+((++B)));  //61
+        System.out.println("PreDecrement of A="+((--A)));  //40
+        System.out.println("PreDecrement of B="+((--B))); //60
+    }
+}
